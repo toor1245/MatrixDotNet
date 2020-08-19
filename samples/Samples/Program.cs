133dcb160801c242f539cf18b74c0169771caf2b
toor1245 <koliagogsadze@gmail.com>
2020-08-20 00:21:57 +0300

    Update Program.cs

--- samples/Samples/Program.cs
+++ samples/Samples/Program.cs
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-﻿using System;
+using MatrixDotNet.Extensions;
+using System;
 using System.Threading.Tasks;
 using MatrixDotNet;
-using MatrixDotNet.Extensions;
 using MatrixDotNet.Extensions.Builder;
 using MatrixDotNet.Extensions.Options;
-
-=======
->>>>>>> ca2c0ee4a092f618bf0ce9d033f565a373a8cd0b
 namespace Samples
 {
     class Program
@@ -15,7 +11,7 @@ namespace Samples
         
         static async Task Main(string[] args)
         {
-<<<<<<< HEAD
+
             Matrix<int> matrix = BuildMatrix.Random<int>(3, 3,-1000,1000);
             Template template = new TemplateMarkdown("title6");
             await matrix.SaveAndOpenAsync(template);
@@ -26,9 +22,6 @@ namespace Samples
             Console.WriteLine(matrix3);
             var test = await template.BinaryOpenAsync<int>();
             Console.WriteLine(test);
-=======
-
->>>>>>> ca2c0ee4a092f618bf0ce9d033f565a373a8cd0b
         }
     }
 }
