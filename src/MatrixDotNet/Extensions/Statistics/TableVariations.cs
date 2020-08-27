b70ab8c69176e523fd16b67aca4b43663453ff55
toor1245 <43444946+toor1245@users.noreply.github.com>
2020-08-27 22:20:58 +0300

    merge fixed

--- src/MatrixDotNet/Extensions/Statistics/TableVariations.cs
+++ src/MatrixDotNet/Extensions/Statistics/TableVariations.cs
@@ -1,11 +1,7 @@
 namespace MatrixDotNet.Extensions.Statistics
 {
     /// <summary>
-<<<<<<< HEAD
     /// Represents columns for matrix variations.
-=======
-    /// Represents columns for matrix.
->>>>>>> 36a87f952f092fbdff24c85b4c16222ed44fb2e0
     /// </summary>
     public enum TableVariations
     {
