62100987ccceabd02e5e9eb6ad0ee56e00743163
Mykola Hohsadze <mykola_hohsadze@epam.com>
2020-10-08 23:24:54 +0300

    add bubble sort

---
+++ src/MatrixDotNet.NetCore/Optimization/Simd/Sorting/QuickSort.cs
@@ -0,0 +1,10 @@
+namespace MatrixDotNet.Extensions.Core.Optimization.Simd.Sorting
+{
+    public static partial class Simd 
+    {
+        public static void QuickSort(this Matrix<int> matrix)
+        {
+            
+        }
+    }
+}
