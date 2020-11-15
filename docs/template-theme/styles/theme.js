4c1967fcce68c24cd16f3cfd7d9ad0d58b15d23c
Dilorfin <pavel.kuybida@gmail.com>
2020-11-15 22:19:11 +0200

    Fix some colors

--- docs/template-theme/styles/theme.js
+++ docs/template-theme/styles/theme.js
@@ -13,15 +13,23 @@ function setLightTheme() {
     let root = document.documentElement;
 
     root.style.setProperty('--color-foreground', "#2d2d30");
-    root.style.setProperty('--color-navbar', "#66666d");
+    
+    root.style.setProperty('--color-navbar-text', "#bfbfbf");
+    root.style.setProperty('--color-navbar-active', "#a6a6a6");
+    root.style.setProperty('--color-navbar-background', "#1e1e1e");
+    
+    root.style.setProperty('--color-subnav-background', "#333337");
+
     root.style.setProperty('--color-breadcrumb', "#999");
-    root.style.setProperty('--color-underline', "#ddd");
-    root.style.setProperty('--color-toc-hover', "#fff");
-    root.style.setProperty('--color-background', "white");
-    root.style.setProperty('--color-background-subnav', "#333337");
+    root.style.setProperty('--color-breadcrumb-hover', "white");
+    
+    root.style.setProperty('--color-toc-hover', "#636369");
+    root.style.setProperty('--color-toc-active', "#337ab7");
+
+    root.style.setProperty('--color-background', "#fff");
     root.style.setProperty('--color-background-input', "white");
     root.style.setProperty('--color-background-dark', "#ddd");
-    root.style.setProperty('--color-background-table-alt', "#212123");
+    root.style.setProperty('--color-background-table-alt', "#f9f9f9");
     root.style.setProperty('--color-background-quote', " #69696e");
 
     replaceHljsStylesheet("dark", "light");
@@ -32,12 +40,20 @@ function setDarkTheme() {
     let root = document.documentElement;
     
 	root.style.setProperty('--color-foreground', "#ccd5dc");
-	root.style.setProperty('--color-navbar', "#66666d");
-	root.style.setProperty('--color-breadcrumb', "#999");
-	root.style.setProperty('--color-underline', "#ddd");
-	root.style.setProperty('--color-toc-hover', "#fff");
+    
+    root.style.setProperty('--color-navbar-text', "#66666d");
+    root.style.setProperty('--color-navbar-active', "#ccd5dc");
+    root.style.setProperty('--color-navbar-background', "#1e1e1e");
+
+    root.style.setProperty('--color-subnav-background', "#333337");
+    
+    root.style.setProperty('--color-breadcrumb', "#999");
+    root.style.setProperty('--color-breadcrumb-hover', "#999");
+	
+    root.style.setProperty('--color-toc-hover', "white");
+    root.style.setProperty('--color-toc-active', "#337ab7");
+    
 	root.style.setProperty('--color-background', "#2d2d30");
-	root.style.setProperty('--color-background-subnav', "#333337");
     root.style.setProperty('--color-background-input', "#333337");
 	root.style.setProperty('--color-background-dark', "#1e1e1e");
 	root.style.setProperty('--color-background-table-alt', "#212123");
