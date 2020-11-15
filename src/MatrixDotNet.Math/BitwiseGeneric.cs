5404f65d83c5c21f8c942a2e6e440a561bd453e4
Mykola Hohsadze <koliagogsadze@gmail.com>
2020-11-16 01:01:42 +0200

    update bitwise generic

--- src/MatrixDotNet.Math/BitwiseGeneric.cs
+++ src/MatrixDotNet.Math/BitwiseGeneric.cs
@@ -119,31 +119,5 @@ namespace MatrixDotNet.Math
         }
 
         #endregion
-
-        #region Not
-
-        private static Func<T1, TR> NotFunc;
-
-        public static Func<T1, TR> GetNotFunc()
-        {
-            if (NotFunc != null)
-                return NotFunc;
-
-            var leftPar = Expression.Parameter(typeof(T1), "left");
-            var resultType = typeof(TR);
-            var body = Expression.Not(Expression.Convert(leftPar, resultType));
-
-            var func = Expression.Lambda<Func<T1, TR>>(body, leftPar).Compile();
-            NotFunc = func;
-
-            return func;
-        }
-
-        public static TR Not(T1 left)
-        {
-            return GetNotFunc()(left);
-        }
-
-        #endregion
     }
 }
