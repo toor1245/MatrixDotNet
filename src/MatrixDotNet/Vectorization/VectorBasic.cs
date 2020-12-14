ec61cf75d0e73af7248121dd2969477d506aa439
Mykola Hohsadze <koliagogsadze@gmail.com>
2020-12-15 00:50:23 +0200

    back

--- src/MatrixDotNet/Vectorization/VectorBasic.cs
+++ src/MatrixDotNet/Vectorization/VectorBasic.cs
@@ -76,7 +76,8 @@ namespace MatrixDotNet.Vectorization
 
             Vector<T> vc = new Vector<T>(len);
             int i = 0;
-            
+
+#if OS_WINDOWS || OS_LINUX
             int size = System.Numerics.Vector<T>.Count;
             int lastIndexBlock = len - len % size;
 
@@ -87,6 +88,7 @@ namespace MatrixDotNet.Vectorization
                 var vectorC = Vector.Subtract(vectorB, vectorA);
                 vectorC.CopyTo(vc.Array);
             }
+#endif
 
             for (; i < vc.Length; i++)
             {
