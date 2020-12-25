14fcae11d64ba8b245e783df0443c1d2351dc14b
Mykola Hohsadze <koliagogsadze@gmail.com>
2020-12-25 18:07:45 +0200

    optimize Strassen multiply parallel

--- src/MatrixDotNet/Extensions/Performance/MultiplyMatrixUnsafe.cs
+++ src/MatrixDotNet/Extensions/Performance/MultiplyMatrixUnsafe.cs
@@ -27,18 +27,18 @@ namespace MatrixDotNet.Extensions.Performance
 
             a.SplitMatrix(out var a11, out var a12, out var a21, out var a22);
             b.SplitMatrix(out var b11, out var b12, out var b21, out var b22);
-
+            
             var p1 = Task.Run(() => MultiplyStrassen(a11 + a22, b11 + b22));
             var p2 = Task.Run(() => MultiplyStrassen(a21 + a22, b11));
             var p3 = Task.Run(() => MultiplyStrassen(a11, b12 - b22));
             var p4 = Task.Run(() => MultiplyStrassen(a22, b21 - b11));
             var p5 = Task.Run(() => MultiplyStrassen(a11 + a12, b22));
             var p6 = Task.Run(() => MultiplyStrassen(a21 - a11, b11 + b12));
-            var p7 = Task.Run(() => MultiplyStrassen(a12 - a22, b21 + b22));
+            var p7 = MultiplyStrassen(a12 - a22, b21 + b22);
 
-            Task.WhenAll(p1, p2, p3, p4, p5, p6, p7);
+            Task.WhenAll(p1, p2, p3, p4, p5, p6);
 
-            var c11 = p1.Result + p4.Result - p5.Result + p7.Result;
+            var c11 = p1.Result + p4.Result - p5.Result + p7;
             var c12 = p3.Result + p5.Result;
             var c21 = p2.Result + p4.Result;
             var c22 = p1.Result + p3.Result - p2.Result + p6.Result;
