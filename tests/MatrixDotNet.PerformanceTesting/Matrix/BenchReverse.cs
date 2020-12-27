452afe94a1d35a4915e7531c7ca87df9e6ff891a
Mykola Hohsadze <koliagogsadze@gmail.com>
2020-12-27 10:43:52 +0200

    update bench

--- tests/MatrixDotNet.PerformanceTesting/Matrix/BenchReverse.cs
+++ tests/MatrixDotNet.PerformanceTesting/Matrix/BenchReverse.cs
@@ -16,7 +16,7 @@ namespace MatrixDotNet.PerformanceTesting.Matrix
         [GlobalSetup]
         public void Setup()
         {
-            _matrix = BuildMatrix.RandomInt(1024, 1024,0, 20);
+            _matrix = BuildMatrix.RandomInt(1024, 1024, 0, 20);
             _matrixByte = BuildMatrix.RandomByte(1024, 1024, 0, 20);
             _matrixFloat = BuildMatrix.RandomFloat(1024, 1024, 0, 20);
             _matrixDouble = BuildMatrix.RandomDouble(1024, 1024, 0, 20);
